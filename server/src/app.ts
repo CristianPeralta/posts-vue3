35c67def813dad47ae9ca5b8ea341a5f1396a4b8
Cristian Peralta <cbperaltasegura@gmail.com>
2023-03-20 15:31:29 -0500

    Replace and refact js files by ts and add command to build the serve application

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,36 @@
 /**  */
+import express from "express";
+import { Response, Request, NextFunction } from "express";
+import indexRoute from  "./routes";
+import morgan from "morgan";
+
+const app = express();
+
+//morgan used for logging
+// app.use(morgan("dev"));
+app.use(morgan<Request, Response>("dev"));
+
+app.use('/', indexRoute);
+
+app.use((error: any, res: Response, next: NextFunction) => {
+    try {
+      const status = error.status || 500;
+      const message =
+        error.message ||
+        "There was an error while processing your request, please try again";
+      return res.status(status).send({
+        status,
+        message,
+      });
+    } catch (error) {
+      next(error);
+    }
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Application started on ${port}...`);
+});
+
+export default app;
---
+++ server/src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router, Response, Request } from "express"
+const router: Router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+    return res.json(200);
+});
+
+export default router;

a8470883d5d7ff97deca704630f6a3a870979241
Cristian Peralta <cbperaltasegura@gmail.com>
2023-03-22 15:20:08 -0500

    Add prettier and extensions to configure with eslint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 /**  */
 import express from "express";
 import { Response, Request, NextFunction } from "express";
-import indexRoute from  "./routes";
+import indexRoute from "./routes/index";
 import morgan from "morgan";
 
 const app = express();
@@ -10,27 +10,25 @@ const app = express();
 // app.use(morgan("dev"));
 app.use(morgan<Request, Response>("dev"));
 
-app.use('/', indexRoute);
+app.use("/", indexRoute);
 
 app.use((error: any, res: Response, next: NextFunction) => {
-    try {
-      const status = error.status || 500;
-      const message =
-        error.message ||
-        "There was an error while processing your request, please try again";
-      return res.status(status).send({
-        status,
-        message,
-      });
-    } catch (error) {
-      next(error);
-    }
+	try {
+		const status = error.status || 500;
+		const message = error.message || "There was an error while processing your request, please try again";
+		return res.status(status).send({
+			status,
+			message,
+		});
+	} catch (error) {
+		next(error);
+	}
 });
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Application started on ${port}...`);
+	console.log(`Application started on ${port}...`);
 });
 
 export default app;

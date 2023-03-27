9ec613a19e239006d2fbeffec9fd7d0d16559197
Cristian Peralta <cbperaltasegura@gmail.com>
2023-03-27 13:41:15 -0500

    Add new compiler option in tsconfig file to allow import files using '@'

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,17 +5,17 @@ const routes: RouteRecordRaw[] = [
         path: "/",
         alias: "/posts",
         name: "posts",
-        component: () => import("../components/PostList.vue"),
+        component: () => import("@/components/PostList.vue"),
     },
     {
         path: "/posts/new",
         name: "posts-new",
-        component: () => import("../components/PostForm.vue"),
+        component: () => import("@/components/PostForm.vue"),
     },
     {
         path: "/posts/:id",
         name: "posts-detail",
-        component: () => import("../components/PostDetail.vue"),
+        component: () => import("@/components/PostDetail.vue"),
     }
 ];
 

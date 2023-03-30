b0c77d2a97ed9904b774bb1fc2a95d44d594f8a3
Cristian Peralta <cbperaltasegura@gmail.com>
2023-03-30 13:42:36 -0500

    Add conditional to show up updatePost button

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -63,7 +63,7 @@
                         </button>
                     </template>
                     <template v-if="post.isEditing">
-                        <button @click="updatePost(post)" class="mr-2">
+                        <button v-if="isEmpty(titleDraft)" @click="updatePost(post)" class="mr-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                             </svg>
@@ -126,7 +126,6 @@
             post.isEditing = !post.isEditing;
             this.titleDraft = post.title
             this.descriptionDraft = post.description
-            // alert(`Edit this Post id => ${id}`);
         },
         updatePost(post: Post) {
             post.isEditing = !post.isEditing;
@@ -139,7 +138,7 @@
              post.isEditing = !post.isEditing;
         },
         isEmpty(str: string) {
-            return str.replace(/^\s+/g, '').length
+            return str.replace(/^\s+/g, '').length;
         },
     },
     components: { PostForm }
